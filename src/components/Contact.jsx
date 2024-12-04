51acde2c87f3b7d8bbdea6d528315f20260a92c6
Dammysaina <dammyadediran94@gmail.com>
2024-12-05 00:11:25 +0100

    skills

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,19 +2,24 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <div
+    <section
       name="contact"
-      className="w-full h-screen bg-[#000000] flex justify-center items-center p-4"
+      className="w-full  bg-[#000000] text-gray-300 flex justify-center items-center "
     >
       <div className="pb-8">
-        <p className="text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300">
-          Contact
+        <p className="text-4xl font-bold inline text-zinc-90 relative sm:text-4xl mt-3 whitespace-nowrap z-10">
+          Let's Talk
+          <span className="absolute text-4xl sm:text-5xl font-bold left-0 -top-4 sm:-top-6 bg-clip-text text-transparent bg-gradient-to-b from-zinc-800 -z-10 md:left-1/2 md:-translate-x-1/2">
+              
+              let's talk
+            </span>
         </p>
-        <p className="text-gray-300 pt-4">
-          Shoot me an email - dammyadediran94@gmail.com
+        
+        <p className="text-gray-300 pt-4 flex items-center">
+          Shoot me an email - etoedia@gmail.com
         </p>
       </div>
-    </div>
+    </section>
   );
 };
 
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,21 +1,24 @@
 import React from "react";
 
-import HTML from "../assets/images/html.png";
-import CSS from "../assets/images/css.png";
+import Flutter from "../assets/images/Flutter.svg";
+import Dart from "../assets/images/Dart.svg";
 import JavaScript from "../assets/images/javascript.png";
 import ReactImg from "../assets/images/react.png";
 // import Node from "../assets/images/node.png";
 import FireBase from "../assets/images/firebase.png";
 import GitHub from "../assets/images/github.png";
 import Tailwind from "../assets/images/tailwind.png";
-import Nextjs from "../assets/images/nextjs.png"
+import Nextjs from "../assets/images/nextjs.png";
+import ANDRIODSTUDIO from "../assets/images/android-studio.svg";
+import XCODE from "../assets/images/Xcode.svg";
+import VISUALSTUDIO from "../assets/images/visual-studio-code-2019.svg"
 
 
 const Skills = () => {
   return (
     <div name="skills" className="bg-[#000000] text-gray-300 w-full  ">
       {/* Container*/}
-      <div className=" mx-auto flex flex-col w-full h-full ">
+      <div className=" mx-auto flex flex-col w-full h-full mb-4">
         <div>
           <p className=" font-bold inline  text-xl">
             Technologies
@@ -24,36 +27,36 @@ const Skills = () => {
 
         <div className="flex gap-4 flex-wrap text-center">
           <div className=" flex gap-1 items-center">
-            <img className="w-4 " src={HTML} alt="HTML icon" />
-            <p className="text-zinc-500 lg:text-xs sm:text-base">HTML</p>
+            <img className="w-4 " src={Flutter} alt="HTML icon" />
+            <p className="text-zinc-500 lg:text-sm sm:text-base">Flutter</p>
           </div>
           <div className="flex gap-1 items-center">
-            <img className="w-4 mx-auto" src={CSS} alt="HTML icon" />
-            <p className="my-4 text-zinc-500 lg:text-xs sm:text-base">CSS</p>
+            <img className="w-4 mx-auto" src={Dart} alt="HTML icon" />
+            <p className="my-4 text-zinc-500 lg:text-sm sm:text-base">Dart</p>
           </div>
           <div className="flex gap-1 items-center">
             <img className="w-4 mx-auto" src={JavaScript} alt="HTML icon" />
-            <p className="my-4 text-zinc-500 lg:text-xs sm:text-base">JAVASCRIPT</p>
+            <p className="my-4 text-zinc-500 lg:text-sm sm:text-base">JAVASCRIPT</p>
           </div>
           <div className="flex gap-1 items-center">
             <img className="w-4 mx-auto" src={ReactImg} alt="HTML icon" />
-            <p className="my-4 text-zinc-500 lg:text-xs sm:text-base">REACT</p>
+            <p className="my-4 text-zinc-500 lg:text-sm sm:text-base">REACT</p>
           </div>
           <div className="flex gap-1 items-center">
             <img className="w-4 mx-auto" src={Nextjs} alt="HTML icon" />
-            <p className="my-4 text-zinc-500 lg:text-xs sm:text-base">NEXT</p>
+            <p className="my-4 text-zinc-500 lg:text-sm sm:text-base">NEXT</p>
           </div>
           <div className="flex gap-1 items-center">
             <img className="w-4 mx-auto" src={GitHub} alt="HTML icon" />
-            <p className="my-4 text-zinc-500 lg:text-xs sm:text-base">GITHUB</p>
+            <p className="my-4 text-zinc-500 lg:text-sm sm:text-base">GITHUB</p>
           </div>
           <div className="flex gap-1 items-center">
             <img className="w-4 mx-auto" src={FireBase} alt="HTML icon" />
-            <p className="my-4 text-zinc-500 lg:text-xs sm:text-base">FIREBASE</p>
+            <p className="my-4 text-zinc-500 lg:text-sm sm:text-base">Firebase</p>
           </div>
           <div className="flex gap-1 items-center">
             <img className="w-4 mx-auto" src={Tailwind} alt="HTML icon" />
-            <p className="my-4 text-zinc-500 lg:text-xs sm:text-base">TAILWIND</p>
+            <p className="my-4 text-zinc-500 lg:text-sm sm:text-base">TAILWIND</p>
           </div>
 
         </div>
@@ -68,36 +71,36 @@ const Skills = () => {
 
         <div className="flex gap-4 flex-wrap text-center">
           <div className=" flex gap-1 items-center">
-            <img className="w-4 " src={HTML} alt="HTML icon" />
-            <p className="text-zinc-500 lg:text-xs sm:text-base">HTML</p>
+            <img className="w-4 " src={ANDRIODSTUDIO} alt="HTML icon" />
+            <p className="text-zinc-500 lg:text-sm sm:text-base">Android Studio </p>
           </div>
           <div className="flex gap-1 items-center">
-            <img className="w-4 mx-auto" src={CSS} alt="HTML icon" />
-            <p className="my-4 text-zinc-500 lg:text-xs sm:text-base">CSS</p>
+            <img className="w-4 mx-auto" src={XCODE} alt="HTML icon" />
+            <p className="my-4 text-zinc-500 lg:text-sm sm:text-base">Xcode</p>
           </div>
           <div className="flex gap-1 items-center">
-            <img className="w-4 mx-auto" src={JavaScript} alt="HTML icon" />
-            <p className="my-4 text-zinc-500 lg:text-xs sm:text-base">JAVASCRIPT</p>
+            <img className="w-4 mx-auto" src={VISUALSTUDIO} alt="HTML icon" />
+            <p className="my-4 text-zinc-500 lg:text-sm sm:text-base">VSCode</p>
           </div>
           <div className="flex gap-1 items-center">
             <img className="w-4 mx-auto" src={ReactImg} alt="HTML icon" />
-            <p className="my-4 text-zinc-500 lg:text-xs sm:text-base">REACT</p>
+            <p className="my-4 text-zinc-500 lg:text-sm sm:text-base">REACT</p>
           </div>
           <div className="flex gap-1 items-center">
             <img className="w-4 mx-auto" src={Nextjs} alt="HTML icon" />
-            <p className="my-4 text-zinc-500 lg:text-xs sm:text-base">NEXT</p>
+            <p className="my-4 text-zinc-500 lg:text-sm sm:text-base">NEXT</p>
           </div>
           <div className="flex gap-1 items-center">
             <img className="w-4 mx-auto" src={GitHub} alt="HTML icon" />
-            <p className="my-4 text-zinc-500 lg:text-xs sm:text-base">GITHUB</p>
+            <p className="my-4 text-zinc-500 lg:text-sm sm:text-base">GITHUB</p>
           </div>
           <div className="flex gap-1 items-center">
             <img className="w-4 mx-auto" src={FireBase} alt="HTML icon" />
-            <p className="my-4 text-zinc-500 lg:text-xs sm:text-base">FIREBASE</p>
+            <p className="my-4 text-zinc-500 lg:text-sm sm:text-base">FIREBASE</p>
           </div>
           <div className="flex gap-1 items-center">
             <img className="w-4 mx-auto" src={Tailwind} alt="HTML icon" />
-            <p className="my-4 text-zinc-500 lg:text-xs sm:text-base">TAILWIND</p>
+            <p className="my-4 text-zinc-500 lg:text-sm sm:text-base">TAILWIND</p>
           </div>
 
         </div>
