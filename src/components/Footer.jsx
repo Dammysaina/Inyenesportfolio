d829ced46b48407181d7c5af9ef2b97bb49ae5bb
Dammysaina <dammyadediran94@gmail.com>
2024-12-05 03:02:41 +0100

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='flex justify-between text-zinc-500 border-t border-[#1a1a1a] px-5 py-8 md:flex-row flex-col gap-4 items-center '>
+    <div className='flex justify-between text-zinc-500 border-t border-[#1a1a1a] px-5 py-8 md:flex-row flex-col gap-4 items-center lg:text-sm sm:text-base'>
         <p>Built with <a href="https://react.dev/" target='_blank' rel='noreferrer' className="hover:text-zinc-500 text-white transition duration-300">React.js</a> and <a href="https://tailwindcss.com/" target='_blank' rel='noreferrer' className="hover:text-zinc-500 text-white transition duration-300">Tailwind CSS</a>, deployed with <a href="https://vercel.com/" target='_blank' rel='noreferrer' className="hover:text-zinc-500 text-white transition duration-300">Vercel</a>.</p>
         <p>Copyright &copy; Dammysaina 2024</p>
     </div>
